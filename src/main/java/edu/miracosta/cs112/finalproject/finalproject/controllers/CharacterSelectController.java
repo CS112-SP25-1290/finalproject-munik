d6b1ee5a9472232ebe43b75b0cea00cf92ea6e72
munik <munikandmom@gmail.com>
2025-03-27 23:05:49 -0700

    added character selection screen

---
+++ src/main/java/edu/miracosta/cs112/finalproject/finalproject/controllers/CharacterSelectController.java
@@ -0,0 +1,36 @@
+package edu.miracosta.cs112.finalproject.finalproject.controllers;
+
+import edu.miracosta.cs112.finalproject.finalproject.Main;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.Button; // Ensure this import is present
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class CharacterSelectController {
+
+    @FXML
+    private Button backButton; // Must match fx:id="backButton" in FXML
+
+    @FXML
+    protected void selectCharacter1() {
+        System.out.println("Character 1 selected");
+    }
+
+    @FXML
+    protected void selectCharacter2() {
+        System.out.println("Character 2 selected");
+    }
+
+    @FXML
+    protected void onBackButtonClick() throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("Player.fxml"));
+        Scene homeScene = new Scene(fxmlLoader.load(), 1280, 720);
+
+        Stage stage = (Stage) backButton.getScene().getWindow();
+        stage.setScene(homeScene);
+        stage.show();
+    }
+}
